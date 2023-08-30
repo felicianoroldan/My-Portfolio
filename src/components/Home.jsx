3ea30d8efd8cc61b6c1aff418a74247d6d779dbf
Roldan Feliciano <roldan.feliciano12@gmail.com>
2023-08-30 15:25:25 +0800

    added animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ import 'animate.css';
 function Home() {
   return (
     <>
-     <div className="container-fluid " style={{ backgroundImage: `url("src/assets/background-img.png")`,
+     <div className="container-fluid " style={{ backgroundImage: `url("./src/assets/background-img.png")`,
       backgroundSize: "cover",
       backgroundRepeat: "no-repeat",
       height: "82vh" }}>
@@ -16,12 +16,12 @@ function Home() {
           <h1 className="hi-there animate__animated animate__bounceInDown animate__delay-1s">Hi There!</h1>
           <h1 className='text-center name animate__animated animate__zoomIn animate__delay-2s'><span className='im'>I'm</span> Roldan Feliciano</h1>
           <h1 className='text-center name animate__animated animate__fadeInRight animate__delay-3s'><span className='im'>A Full-Stuck Web Developer</span></h1>
-          <h5 className='text-center creating animate__animated animate__fadeInUpBig animate__delay-4s'>"Creating interactive and user-friendly web applications. Expertise in front-end development,
+          <h5 className='text-center creating animate__animated animate__bounceInDown animate__delay-4s'>"Creating interactive and user-friendly web applications. Expertise in front-end development,
             back-end programming, and data management. Deliver high-quality solutions that meets clients needs.
             Please feel free to explore my portfolio."
           </h5>
           <div  className='text-center about-me'>
-            <button className='rounded-3 animate__animated animate__fadeInUpBig animate__delay-5s'><span className='p-5 fs-2'>About Me</span></button>
+            <button className='rounded-3 animate__animated animate__backInLeft animate__delay-5s'><span className='p-5 fs-2'>About Me</span></button>
           </div>
         </div>
         <div className="col-12 col-md-6">
