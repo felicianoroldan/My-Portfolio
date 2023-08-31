dcb73dca078400dc01abc0b071e03b8600f76a91
Roldan Feliciano <roldan.feliciano12@gmail.com>
2023-08-31 15:34:01 +0800

    initial commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,19 +13,19 @@ function Home() {
       height: "82vh" }}>
       <div className="row ">
         <div className="col-12 col-md-6">
-          <h1 className="hi-there animate__animated animate__bounceInDown animate__delay-1s">Hi There!</h1>
+          <h1 className="hi-there animate__animated animate__zoomIn animate__delay-2s">Hi There!</h1>
           <h1 className='text-center name animate__animated animate__zoomIn animate__delay-2s'><span className='im'>I'm</span> Roldan Feliciano</h1>
-          <h1 className='text-center name animate__animated animate__fadeInRight animate__delay-3s'><span className='im'>A Full-Stuck Web Developer</span></h1>
-          <h5 className='text-center creating animate__animated animate__bounceInDown animate__delay-4s'>"Creating interactive and user-friendly web applications. Expertise in front-end development,
+          <h1 className='text-center name animate__animated animate__zoomIn animate__delay-2s'><span className='im'>A Full-Stuck Web Developer</span></h1>
+          <h5 className='text-center creating animate__animated animate__zoomIn animate__delay-2s'>"Creating interactive and user-friendly web applications. Expertise in front-end development,
             back-end programming, and data management. Deliver high-quality solutions that meets clients needs.
             Please feel free to explore my portfolio."
           </h5>
           <div  className='text-center about-me'>
-            <button className='rounded-3 animate__animated animate__backInLeft animate__delay-5s'><span className='p-5 fs-2'>About Me</span></button>
+            <button className='rounded-3 animate__animated animate__zoomIn animate__delay-2s'><span className='p-5 fs-2'>About Me</span></button>
           </div>
         </div>
         <div className="col-12 col-md-6">
-          <img src={ fel } alt="Dan-Photo" width="645" className='fel-img animate__animated animate__fadeInLeftBig animate__delay-10s'/>
+          <img src={ fel } alt="Dan-Photo" width="645" className='fel-img animate__animated animate__fadeInLeftBig animate__delay-1s'/>
         </div>
       </div>
      </div>
